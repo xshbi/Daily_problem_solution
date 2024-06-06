8652b84eb07049bd7802a03f055ccda6df55c970
Akashvi <109546133+xshbi@users.noreply.github.com>
2024-06-06 15:36:15 +0530

    Add files via upload
    Even or Odd Checker
    Description
    This C++ program is designed to determine whether a given number is even or odd. It provides a simple and efficient way to check the parity of any integer input. 🧮
    
    How It Works
    The program prompts the user to input a number, after which it performs a modulo operation to check whether the number is even or odd. If the input number is evenly divisible by 2, it is classified as even; otherwise, it is considered odd. 🔄

---
+++ oddeven.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int x;
+    cout<<"type the no";
+    cin>>x;
+    if (x%2==0)
+    {
+        cout<<"this is even no";
+    }
+    else
+    {
+        cout<<"This is an odd no";
+    }
+    return 0;
+}
+
+
+
