4c5e65f6094d612dfcb80cadbce340b9c16b4090
Akashvi <109546133+xshbi@users.noreply.github.com>
2024-06-18 23:38:03 +0530

    Create comparision_of_no.py
    This is basic example of nested if else statement. where we take 3 input and compare.

---
+++ comparision_of_no.py
@@ -0,0 +1,9 @@
+num1 = int(input("Enter the number:"))
+num2 = int(input("Enter the number:"))
+num3 = int(input("Enter the number:"))
+if num1>num2:
+  if num2>num3:
+    print(num1,"is greater than ",num2,"and",num3)
+else : 
+  print(num1,"is less ",num2," and ",num3")
+print("end of nested if")
