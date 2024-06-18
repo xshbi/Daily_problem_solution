cc9692f54a5b0db647b8aa5da78d21b45257c2e5
Akashvi <109546133+xshbi@users.noreply.github.com>
2024-06-18 23:40:34 +0530

    comparision_of_no.py
    This is basic example of nested statement. Here we compare the input of 3 no.

--- comparision_of_no.py
+++ comparision_of_no.py
@@ -1,6 +1,13 @@
 num1 = int(input("Enter the number:"))
+
+
+
 num2 = int(input("Enter the number:"))
+
+
+
 num3 = int(input("Enter the number:"))
+
 if num1>num2:
   if num2>num3:
     print(num1,"is greater than ",num2,"and",num3)
