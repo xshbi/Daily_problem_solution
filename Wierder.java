a23fa03b333f2a1a21df1e66b9d326dcad895a93
Akashvi <109546133+xshbi@users.noreply.github.com>
2025-01-28 21:45:15 +0530

    Create Wierder.java
    This is a java program for that print wierd for odd and not weird on odd with in defined range.

---
+++ Wierder.java
@@ -0,0 +1,14 @@
+Public Class Wierder{
+  Public Static void main(String[] args)
+  Scanner scanner = new Scanner(system.in);
+  int N = Scanner.nextInt();
+
+if(N%2 ==0 && N>2 && N<5){
+  System.out.println("Not Wierd");
+else if(N%2 == 0 && N>6 && N<20){
+  System.out.println("Wierd");}
+else if(N%2 == 0) {
+  System.out.println("Not Wierd");}
+else{
+  System.out.println("wierd");}
+}}
