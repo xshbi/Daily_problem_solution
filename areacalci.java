3cc8d895be4420502bd2c93aa8431b07390a995b
Akashvi <109546133+xshbi@users.noreply.github.com>
2025-01-29 21:21:50 +0530

    Create areacalci.java
    Here we created a java function that helps us to calculate the area of the rectangle

---
+++ areacalci.java
@@ -0,0 +1,16 @@
+//We need to write a java functions named "calculate Area" that takes into parameters the length and width of a rectangle as integer and return the area of this rectangle.
+
+
+
+Public class areacalci {
+//This is a public class
+  public static void main(String[] args)
+          //void is something that doesn't return
+  {System.out.println(calculate Area(10,20));}
+            //These value entered by the program
+  public static int calculateArea (int l,int w){
+    //Here int l,w are the parameters written as params
+    //Area is f(x) where l is length and w is the width of the parameter
+  int Area = l*w;
+    
+  return Area;}
