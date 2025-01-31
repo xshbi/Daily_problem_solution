905ff246789468d0be03f38ed4e3b6e72c533720
Akashvi <109546133+xshbi@users.noreply.github.com>
2025-01-31 21:21:36 +0530

    Create Understanding_array.java
    Here we are trying to define the array.

---
+++ Understanding_array.java
@@ -0,0 +1,18 @@
+public class Understanding_array{
+  public static void main(String[] args){
+
+  int[] numberArray = new int[5];
+
+
+
+  numbers Array[0] = 10;
+  numbers Array[1] = 20;
+  numbers Array[2] = 30;
+  numbers Array[3] = 40;
+  numbers Array[4] = 50;
+
+
+
+//Here we defining the array and there defining the value of the value
+  System.out.println("element index "+ numbersArray[0]);
+    //Here we print the element number 
