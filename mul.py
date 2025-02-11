d14a2a981925cf9f68efc5d21edb2b4a6c7aeb11
Akashvi <109546133+xshbi@users.noreply.github.com>
2025-02-11 14:41:01 +0530

    Create mul.py
    This is code for taking the product of x and y in range of 1,11.

---
+++ mul.py
@@ -0,0 +1,5 @@
+for x in range(1, 11):  
+    for y in range(1, 11):  
+        print(x * y, end='')  
+    print()
+#here we running the loop and form 1 to 11 for x and nested loop for y from 1 to 11 and printing the product of the following. 
